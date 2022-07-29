fc4e9d1ffe3426c15c054c4ad90e77ca14e2fc0d
Bad-Guy23 <neiltimothysy23@gmail.com>
2022-07-29 15:53:50 +0800

    Delete Teacher and Update Teacher

--- functions/index.js
+++ functions/index.js
@@ -2,16 +2,19 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 admin.initializeApp();
 
+// delete teacher
 exports.deleteTeacher = functions.firestore.document("users/{uid}")
     .onDelete((snap, context) => {
       return admin.auth().deleteUser(context.params.uid);
     });
 
+// update or edit Teacher
 exports.updateTeacher = functions.firestore.document("users/{uid}")
     .onUpdate((snap, context) => {
       const newValues = snap.after.data();
       const previousValues = snap.before.data();
 
+      // update email from auth
       if (newValues.email != previousValues.email) {
         return admin.auth().updateUser(context.params.uid, {
           email: newValues.email,
